31b7dcd43d6cf0ea6d6a64ca11ad4c357e6c6d15
Ajnana1997 <19mp1436@rit.ac.in>
2021-12-23 23:26:09 +0530

    Chating

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,262 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'FriendlyChat',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FriendlyChat'),
-        ),
-        body: _buildTextComposer(),
-      ),
-    ),
+    FriendlyChatApp(),
   );
 }
 
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+      .copyWith(secondary: Colors.orangeAccent[400]),
+);
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // MODIFIED
+      title: 'FriendlyChat',
+      theme: defaultTargetPlatform == TargetPlatform.iOS // NEW
+          ? kIOSTheme // NEW
+          : kDefaultTheme, // NEW
+      home: const ChatScreen(), // MODIFIED
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage(
+      {required this.text, required this.animationController, Key? key})
+      : super(key: key);
+  final String text;
+  final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    String _name = 'User Name';
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(_name, style: Theme.of(context).textTheme.headline4),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = [];
+  final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  bool _isComposing = false;
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onChanged: (text) {
+                  // NEW
+                  setState(() {
+                    // NEW
+                    _isComposing = text.isNotEmpty; // NEW
+                  }); // NEW
+                },
+                onSubmitted: _isComposing ? _handleSubmitted : null,
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Send a message'),
+                focusNode: _focusNode,
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? // MODIFIED
+                    CupertinoButton(
+                        // NEW
+                        child: const Text('Send'), // NEW
+                        onPressed: _isComposing // NEW
+                            ? () =>
+                                _handleSubmitted(_textController.text) // NEW
+                            : null,
+                      )
+                    : // NEW
+                    IconButton(
+                        // MODIFIED
+                        icon: const Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    setState(() {
+      // NEW
+      _isComposing = false; // NEW
+    });
+    var message = ChatMessage(
+      // NEW
+      text: text,
+      animationController: AnimationController(
+        // NEW
+        duration: const Duration(milliseconds: 700), // NEW
+        vsync: this, // NEW
+      ), // NEW                  // NEW
+    ); // NEW
+    setState(() {
+      // NEW
+      _messages.insert(0, message); // NEW
+    });
+    _focusNode.requestFocus(); // NEW
+    message.animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FriendlyChat')),
+      body: Column(
+        // MODIFIED
+        children: [
+          // NEW
+          Flexible(
+            // NEW
+            child: ListView.builder(
+              // NEW
+              padding: const EdgeInsets.all(8.0), // NEW
+              reverse: true, // NEW
+              itemBuilder: (_, index) => _messages[index], // NEW
+              itemCount: _messages.length, // NEW
+            ), // NEW
+          ), // NEW
+          const Divider(height: 1.0), // NEW
+          Container(
+            // NEW
+            decoration:
+                BoxDecoration(color: Theme.of(context).cardColor), // NEW
+            child: _buildTextComposer(), // MODIFIED
+          ), // NEW
+        ], // NEW
+      ),
+       // NEW
+    );
+  }
+}
 
 final _textController = TextEditingController();
 
+class BuildText extends StatelessWidget {
+  const BuildText({Key? key}) : super(key: key);
 
-Widget _buildTextComposer() {
-  return Container(
-    margin: const EdgeInsets.symmetric(horizontal: 8.0),
-   child: Row(                
-      children: [              
-        Flexible(              
-          child: TextField(
-            controller: _textController,
-            onSubmitted: _handleSubmitted,
-            decoration:
-                const InputDecoration.collapsed(hintText: 'Send a message'),
-          ),
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
         ),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 4.0),
-          child: IconButton(
-          icon: const Icon(Icons.send),
-          onPressed: () => _handleSubmitted(_textController.text)), 
-        ),                     
-      ],                       
-    ),         
-  );
+      ),
+    );
+  }
 }
 
 void _handleSubmitted(String text) {
   _textController.clear();
 }
 
-
-
-// Flutter dev part 1 to 2 Start 
+// Flutter dev part 1 to 2 Start
 class RandomWords extends StatefulWidget {
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];                  // wrod pair list from the external package
+  final _suggestions = <WordPair>[]; // wrod pair list from the external package
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    //final wordPair = WordPair.random(); // Delete these... 
+    //final wordPair = WordPair.random(); // Delete these...
     //return Text(wordPair.asPascalCase); // ... two lines.
 
-    return Scaffold (                     // Add from here... 
+    return Scaffold(
+      // Add from here...
       appBar: AppBar(
-        title: Text('Startup Name Generator'), actions: [
+        title: Text('Startup Name Generator'),
+        actions: [
           IconButton(
             icon: const Icon(Icons.list),
             onPressed: _pushSaved,
@@ -75,12 +265,10 @@ class _RandomWordsState extends State<RandomWords> {
         ],
       ),
       body: _buildSuggestions(),
-    );                                      // ... to here.
+    ); // ... to here.
   }
 
-
-
-    void _pushSaved() {
+  void _pushSaved() {
     Navigator.of(context).push(
       // Add lines from here...
       MaterialPageRoute<void>(
@@ -113,65 +301,62 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
-
-  
-   Widget _buildRow(WordPair pair) {
+  Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
-      trailing: Icon(    
-      alreadySaved ? Icons.favorite : Icons.favorite_border,
-      color: alreadySaved ? Colors.red : null,
-      semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-      
-    ),
-    onTap: () {       
-      setState(() {
-        if (alreadySaved) {
-          _saved.remove(pair);
-        } else { 
-          _saved.add(pair); 
-        } 
-      });
-    },   
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
-   Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(10),
-      // The itemBuilder callback is called once per suggested 
-      // word pairing, and places each suggestion into a ListTile
-      // row. For even rows, the function adds a ListTile row for
-      // the word pairing. For odd rows, the function adds a 
-      // Divider widget to visually separate the entries. Note that
-      // the divider may be difficult to see on smaller devices.
-      itemBuilder: (BuildContext _context, int i) {
-        // Add a one-pixel-high divider widget before each row 
-        // in the ListView.
-        if (i.isOdd) {
-          return Divider();
-        }
 
-        // The syntax "i ~/ 2" divides i by 2 and returns an 
-        // integer result.
-        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-        // This calculates the actual number of word pairings 
-        // in the ListView,minus the divider widgets.
-        final int index = i ~/ 2;
-        // If you've reached the end of the available word
-        // pairings...
-        if (index >= _suggestions.length) {
-          // ...then generate 10 more and add them to the 
-          // suggestions list.
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(10),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 }
 
-// Flutter dev part 1 to 2 End 
+// Flutter dev part 1 to 2 End
