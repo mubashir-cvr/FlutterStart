b07df5fef6830db63722b6c675ce4477cd6080e0
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-10 23:16:33 +0530

    adding login page

--- lib/login.dart
+++ lib/login.dart
@@ -23,6 +23,9 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,11 +42,58 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             const SizedBox(height: 120.0),
-            // TODO: Wrap Username with AccentColorOverride (103)
+                       // TODO: Wrap Username with AccentColorOverride (103)
+
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
             // TODO: Add TextField widgets (101)
+              TextField(
+               controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+              
+            ),
+// spacer
+            const SizedBox(height: 12.0),
+// [Password]
+             TextField(
+               controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+
             // TODO: Add button bar (101)
+            ButtonBar(
+              // ignore: todo
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // TODO: Add buttons (101)
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    // ignore: todo
+                    // TODO: Clear the text fields (101)
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                    // ignore: todo
+                    // TODO: Show the next page (101)
+                      Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
