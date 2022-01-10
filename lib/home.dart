62ce1db565af137084c8b986208397bce28514c7
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-10 23:28:34 +0530

    MDC-102 4

--- lib/home.dart
+++ lib/home.dart
@@ -23,8 +23,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      // TODO: Add app bar (102)
+    return Scaffold(
+       // TODO: Add app bar (102)
+  appBar: AppBar(
+    leading: IconButton(
+      icon: const Icon(
+        Icons.menu,
+        semanticLabel: 'menu',
+      ),
+      onPressed: () {
+        print('Menu button');
+      },
+    ),
+     title: const Text('SHRINE'),
+     actions: <Widget>[
+  IconButton(
+    icon: const Icon(
+      Icons.search,
+      semanticLabel: 'search',
+    ),
+    onPressed: () {
+      print('Search button');
+    },
+  ),
+  IconButton(
+    icon: const Icon(
+      Icons.tune,
+      semanticLabel: 'filter',
+    ),
+    onPressed: () {
+      print('Filter button');
+    },
+  ),
+],
+    // TODO: Add buttons and title (102)
+  ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
